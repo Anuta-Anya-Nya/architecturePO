592da08eaafdca6ace5d41668215d8f92d555917
Aeshe <syslichek@mail.ru>
2023-09-28 23:48:24 +0300

    add HW1 - editor3D

---
+++ s1/editor3d/src/ModelElements/Flash.java
@@ -0,0 +1,16 @@
+package ModelElements;
+
+import java.awt.Color;
+
+import Staff.Angle3D;
+import Staff.Point3D;
+
+public class Flash {
+    public Point3D location;
+    public Angle3D angle;
+    public Color color;
+    public Float Power;
+
+    public void Rotate(Angle3D angle) {}
+    public void Move(Point3D point){}
+}
---
+++ s1/editor3d/src/ModelElements/Poligon.java
@@ -0,0 +1,9 @@
+package ModelElements;
+
+import java.util.List;
+
+import Staff.Point3D;
+
+public class Poligon {
+    public List<Point3D> points;
+}
